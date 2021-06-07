824e4abf3a340bc0405354fccd93435bbc44730c
Thais <thais_fernandes62@hotmail.com>
2021-06-07 19:13:38 -0300

    --Botão definir excluído do html e feito função cifrar com bug

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,20 @@
 const cipher = {
-  // ...
-};
+    encode: function(offset, string){
+    
+    let array = string.split("");
+    let str = "";
 
+    array.forEach(function (e){
+
+    let indice2 = ((e.charCodeAt() - 65 + offset) % 26) + 65;
+    let cifradaLetra = String.fromCharCode(indice2);
+    str = str.concat(cifradaLetra); 
+    }); return str;
+    },
+  
+    decode: function(offset, string){
+
+    }};
+
+  
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+   /* let button = document.getElementById('botao');
+    button.addEventListener('click', event => {
+        event.preventDefault();
+        let input = document.getElementById('des').value;
+        console.log(input);
+    })*/
+    /*let input = document.getElementById('des');
+    input.addEventListener("keypress",event => {
+        input = (event.target.value);
+    });
+    */
+
+    /*let string_tocifer = document.getElementById('msg_tocifer');
+    string_tocifer.addEventListener("keypress",event => {
+        string_tocifer = (event.target.value);
+    });*/
+
+
+    //let string_todecifer = document.getElementById('msg_todecifer').value;
+
+
+
+
+    const btn = document.getElementById('to_cifer');
+    function cifer(e){
+        e.preventDefault();
+        let input = document.getElementById('des').value;
+        console.log(input);
+        let string_tocifer = document.getElementById('msg_tocifer').value;
+        console.log(string_tocifer);
+        let textoCifrado = cipher.encode(3, "FRASE UM");
+        console.log(textoCifrado);
+        let caixaCifrada = document.getElementById('msg_cifered');
+        caixaCifrada.innerHTML = textoCifrado;
+    }
+
+    btn.addEventListener("click", (e) => cifer(e));
+
+
+//let textoCifrado = cipher.encode(3, "FRASE UM");
+//console.log(textoCifrado);
+
+//let caixaCifrada = document.getElementById('msg_cifered');
+//caixaCifrada.innerHTML = textoCifrado;
