4150aa70d0518671d841e2e30e572a89e6378103
Thais <thais_fernandes62@hotmail.com>
2021-06-08 19:02:08 -0300

    Função decifrar adicionada

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,28 @@
 const cipher = {
-    encode: function(offset, string){
-    
-    let array = string.split("");
-    let str = "";
-    let offset_n = parseInt(offset);
+    encode: function (offset, string) {
+        let array = string.split("");
+        let str = "";
+        let offset_n = parseInt(offset);
 
-    array.forEach(function (e){
-
-    let indice2 = ((e.charCodeAt() - 65 + offset_n) % 26) + 65;
-    let cifradaLetra = String.fromCharCode(indice2);
-    str = str.concat(cifradaLetra); 
-    }); return str;
+        array.forEach(function (e) {
+            let indice2 = ((e.charCodeAt() - 65 + offset_n) % 26) + 65;
+            let cifradaLetra = String.fromCharCode(indice2);
+            str = str.concat(cifradaLetra);
+        }); return str;
     },
-  
-    decode: function(offset, string){
 
-    }};
+    decode: function (offset, string) {
+        let array2 = string.split("");
+        let str = "";
+        let offset_n = (offset);
 
-  
+        array2.forEach(function (e) {
+            let indice = ((e.charCodeAt() - 65 - offset_n) % 26) + 65;
+            let cifradaLetra2 = String.fromCharCode(indice);
+            str = str.concat(cifradaLetra2);
+        }); return str;
+    }
+};
+
+
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,32 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+const btn = document.getElementById('to_cifer');
+function cifer(e) {
+    e.preventDefault();
+    let input = document.getElementById('des').value;
+    console.log(input);
+    let string_tocifer = document.getElementById('msg_tocifer').value;
+    console.log(string_tocifer);
+    let textoCifrado = cipher.encode(input, string_tocifer);
+    console.log(textoCifrado);
+    let caixaCifrada = document.getElementById('msg_cifered');
+    caixaCifrada.innerHTML = textoCifrado;
+}
 
+btn.addEventListener("click", (e) => cifer(e));
 
+const btn2 = document.getElementById('to_decifer');
+function decifer(e) {
+    e.preventDefault();
+    let input = document.getElementById('des').value;
+    console.log(input);
+    let string_todecifer = document.getElementById('msg_todecifer').value;
+    console.log(string_todecifer);
+    let textoDecifrado = cipher.decode(input, string_todecifer);
+    console.log(textoDecifrado);
+    let CaixaDecifrada = document.getElementById('msg_decifered');
+    CaixaDecifrada.innerHTML = textoDecifrado;
+}
 
-    //let string_todecifer = document.getElementById('msg_todecifer').value;
-
-
-
-
-    const btn = document.getElementById('to_cifer');
-    function cifer(e){
-        e.preventDefault();
-        let input = document.getElementById('des').value;
-        console.log(input);
-        let string_tocifer = document.getElementById('msg_tocifer').value;
-        console.log(string_tocifer);
-        let textoCifrado = cipher.encode(input, string_tocifer);
-        console.log(textoCifrado);
-        let caixaCifrada = document.getElementById('msg_cifered');
-        caixaCifrada.innerHTML = textoCifrado;
-    }
-
-    btn.addEventListener("click", (e) => cifer(e));
+btn2.addEventListener("click", (e) => decifer(e));
